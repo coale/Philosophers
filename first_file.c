3e12a97830b11b3aefc75f182a24e0c374c9ede9
Alessandra Conta <aconta@c1r2p1.42firenze.it>
2023-06-12 14:05:51 +0200

    learning

--- first_file.c
+++ first_file.c
@@ -1,4 +1,41 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_file.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aconta <aconta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 10:23:50 by aconta            #+#    #+#             */
+/*   Updated: 2023/06/12 10:23:50 by aconta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "philo.h"
 
+void *helloWorld()
+{
+    sleep(1);
+    printf("Hello World \n");
+    return NULL;
+}
+
+/* int philo_create()
+{
+
+} */
+
+int main()
+{
+    pthread_t thread_id;
+    int i = 0;
+
+    while (i < 3)
+    {
+        printf("Before Thread %d\n", i);
+        pthread_create(&thread_id, NULL, helloWorld, NULL);
+        //pthread_join(thread_id, NULL);
+        printf("After Thread\n");
+        i++;
+    }
+    exit(0);
+}
--- philo.h
+++ philo.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aconta <aconta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 10:23:55 by aconta            #+#    #+#             */
+/*   Updated: 2023/06/12 10:23:55 by aconta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 
 #ifndef PHILO_H
@@ -9,7 +21,7 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct s_fork
+/* typedef struct s_fork
 {
     t_fork right;
     t_fork left;
@@ -19,13 +31,17 @@ typedef struct s_philo
 {
     int id;
     t_fork fork;
+    pthread thread;
 } t_philo;
 
 typedef struct s_args
 {
     int number_of_philosophers;
-    uint64_t time_to_die;
-    uint64_t time_to_eat;
-    uint64_t time_to_sleep;
+    long long time_to_die;
+    long long time_to_eat;
+    long long time_to_sleep;
     int number_of_times_each_philosopher_must_eat;
 } t_args;
+*/
+
+# endif
